04753d2b7b583456733bc685433d67dbdb9c1ba1
石桥码农 <oncwnuOujt49B7r_u4KtT4TdWJ5c@git.weixin.qq.com>
2020-08-15 15:11:43 +0800

    2020-08-15:

---
+++ server/routers/goods-router.js
@@ -0,0 +1,18 @@
+const Router = require("@koa/router")
+const GoodsCatetory = require("../models/goods-category-model")
+
+const router = new Router({
+  prefix:"/goods"
+})
+
+router.get("/categories", async function(ctx){
+  let categories = await GoodsCatetory.findAll()
+  ctx.status = 200
+  ctx.body = {
+    coce:200,
+    msg:'ok',
+    data:categories
+  }
+})
+
+module.exports = router
