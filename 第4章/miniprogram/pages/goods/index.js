cf9b43ec260a15d22a851441f0470e396965e4a4
李艺 <liyi@rixingyike.com>
2020-09-13 12:23:31 +0800

    2020-09-13:

--- 第4章/miniprogram/pages/goods/index.js
+++ 第4章/miniprogram/pages/goods/index.js
@@ -9,7 +9,11 @@ Page({
     goodsId:0,
     goodsData:{},
     goodsImages: [],
-    goodsContentInfo:{}
+    goodsContentInfo:{},
+    goodsSkuData:{},
+    selectedGoodsSku:{},
+    selectedAttrValue:{},
+    selectedGoodsSkuObject:{}
   },
 
     // 显示规格面板
@@ -25,8 +29,9 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.data.goodsId = options.goodsId
+  onLoad: async function (options) {
+    let goodsId = options.goodsId
+    this.data.goodsId = goodsId
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('goodsData', (res)=> {
       console.log(res)
@@ -38,6 +43,76 @@ Page({
         goodsImages,
         goodsContentInfo
       })
+    })
+    // 拉取sku规格数据
+    let goodsSkuDataRes = await wx.wxp.request({
+      url: `http://localhost:3000/goods/goods/${goodsId}/sku`,
+    })
+    if (goodsSkuDataRes){
+      let goodsSkuData = goodsSkuDataRes.data.data 
+      this.setData({
+        goodsSkuData
+      })
+    }
+  },
+
+  onTapSkuTag(e){
+    // 获取及设置选择的规格
+    let attrvalue = e.currentTarget.dataset.attrvalue
+    let attrKey = e.currentTarget.dataset.attrkey
+
+    console.log('attrvalueid',attrvalue,attrKey);
+    let selectedAttrValue = this.data.selectedAttrValue
+    selectedAttrValue[attrKey] = attrvalue
+    this.setData({
+      selectedAttrValue
+    })
+    // 计算价格及库存
+    let totalIdValue = 0
+    let goodsAttrKeys = this.data.goodsSkuData.goodsAttrKeys
+    for (let j=0;j<goodsAttrKeys.length;j++){
+      let attrKey = goodsAttrKeys[j].attr_key
+      if (selectedAttrValue[attrKey]){
+        totalIdValue+=selectedAttrValue[attrKey].id
+      }
+    }
+    console.log("totalIdValue", totalIdValue);
+    
+    let goodsSku = this.data.goodsSkuData.goodsSku
+    let tempTotalIdValue = 0
+
+    for(let j=0;j<goodsSku.length;j++){
+      let goodsAttrPath = goodsSku[j].goods_attr_path 
+      if (goodsAttrPath.length != goodsAttrKeys.length){
+        break
+      }
+      tempTotalIdValue = 0
+      goodsAttrPath.forEach(item=>tempTotalIdValue += item)
+      console.log("tempTotalIdValue",tempTotalIdValue);
+
+      if (tempTotalIdValue == totalIdValue){
+        let selectedGoodsSku = goodsSku[j]
+        this.setData({
+          selectedGoodsSku
+        })
+        break;
+      }
+    }
+  },
+
+  // 确定选择当前规格
+  onConfirmGoodsSku(){
+    let goodsSkuData = this.data.goodsSkuData
+    let selectedGoodsSkuObject = this.data.selectedGoodsSkuObject
+    selectedGoodsSkuObject.sku = Object.assign({}, this.data.selectedGoodsSku)
+    selectedGoodsSkuObject.text = ''
+    for (let j=0;j<goodsSkuData.goodsAttrKeys.length;j++){
+      let item = goodsSkuData.goodsAttrKeys[j]
+      selectedGoodsSkuObject.text += this.data.selectedAttrValue[item.attr_key].attr_value + ' '
+    }
+    this.setData({
+      selectedGoodsSkuObject,
+      showSkuPanel: false
     })
   },
 
