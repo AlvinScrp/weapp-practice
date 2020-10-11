b0716c603bd4d4b85076ee26c09b06482e83b5c6
李艺 <liyi@rixingyike.com>
2020-10-11 21:46:13 +0800

    2020-10-11:

--- 第4章/miniprogram/pages/address-list/index.js
+++ 第4章/miniprogram/pages/address-list/index.js
@@ -5,13 +5,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-    selectedAddressId:0
+    selectedAddressId: 0,
+    addressList:[
+      {
+        id:0,
+        user_name:'张三',
+        tel_number:'13800000000',
+        address_info:'新港中路397号'
+      },
+      {
+        id:1,
+        user_name:'李四',
+        tel_number:'13800000000',
+        address_info:'新港中路123号'
+      }
+    ]
   },
 
-  onAddressIdChange(e){
-    let id = e.detail 
+  // 从微信获取收货地址
+  getAddressFromWeixin() {
+    if (wx.canIUse('chooseAddress.success.userName')) {
+      wx.chooseAddress({
+        success: (res) => {
+          console.log(res);
+
+          let addressList = this.data.addressList
+          let addressContained = addressList.find(item=>item.tel_number == res.telNumber)
+          console.log(addressContained);
+          
+          if (addressContained){
+            this.setData({
+              selectedAddressId:addressContained.id
+            })
+            return 
+          }
+
+          let item = {
+            id:addressList.length,
+            user_name:res.userName,
+            tel_number:res.telNumber,
+            address_info:`${res.provinceName}${res.cityName}${res.countyName}${res.detailInfo}`
+          }
+          addressList.push(item)
+          this.setData({
+            addressList,
+            selectedAddressId:item.id
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '可以手动编辑地址',
+      })
+    }
+
+  },
+
+  onAddressIdChange(e) {
+    let id = e.detail
     this.setData({
-      selectedAddressId:id
+      selectedAddressId: id
     })
   },
   /**
