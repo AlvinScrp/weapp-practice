9c3f4958b3d36d3de3b970c6149ea97fc3dd97f3
李艺 <liyi@rixingyike.com>
2020-11-09 16:13:08 +0800

    2020-11-09:

--- server/routers/pay.js
+++ server/routers/pay.js
@@ -1,37 +1,56 @@
 const {wepay:WechatPay} = require('koa3-wechat');
 const short = require('short-uuid');
-const fs = require('fs');
-
-// 普通商户的参数
-let config = {
-  appId: 'wx7...................', //服务商APPID，邮件中
-  mchId: '14........',
-  notifyUrl: 'http://<你的域名>/apis/pay_notify', // 设置到微信支付后台
-  partnerKey: '1234567890abcdefghijklmnopqrstuv', // 设置成你自己的key，在pay.weixin.qq.com设置
-  pfx: fs.readFileSync(__dirname + '/apiclient_cert.p12'),
-  passphrase: '14........'
-}
-
-// 初始化
-let wepay = new WechatPay(config);
+// const fs = require('fs');
+const wepay = require("../lib/wepay")
 
 function init(router) {
   // post /user/my/order
-  router.post('/my/order', async ctx => {
-    let {openId,uid:userId} = ctx.user
+  // 下新定单，准备支付
+  router.post('/my/order', async ctx=>{
+    let {uid:userId,openId} = ctx.user
+    let { totalFee,addressId,addressDesc,goodsCartsIds,goodsNameDesc } = ctx.request.body
+    // 为测试方便，所有金额支付数均为1分
+    totalFee = 1 
+    let payState = 0
+    // 依照Order模型接收参数
+    let outTradeNo = `${new Date().getFullYear()}${short().new()}`
     // 获取订单的预支付信息
     var trade = {
-      body: '描述信息', //最长127字节
-      attach: '附加信息', //最长127字节
-      out_trade_no: '<订单ID，一般用订单表的唯一ID或UUID>', //
-      total_fee: '<订单总价>', //以分为单位，货币的最小金额
-      trade_type: 'JSAPI',
-      spbill_create_ip: '<发起支付的IP>', //ctx.request.ip
+      body: goodsNameDesc.substr(0,127), //最长127字节
+      attach: '支付测试', //最长127字节
+      out_trade_no: outTradeNo, //
+      total_fee: totalFee, //以分为单位，货币的最小金额
+      trade_type: 'JSAPI',//NATIVE
+      spbill_create_ip: ctx.request.ip, //ctx.request.ip
       openid:openId
     };
     var params = await wepay.getBrandWCPayRequestParams(trade);
-    if (!params || !(params.package && params.paySign)) {
-      console.log('error! getBrandWCPayRequestParams() return null!');
+    let err = '', res
+    if (params && params.package && params.paySign) {
+      // 创建记录
+      res = await Order.create({
+        userId,
+        outTradeNo,
+        payState,
+        totalFee,
+        addressId,
+        addressDesc,
+        goodsCartsIds,
+        goodsNameDesc
+      })
+      if (!res) err = 'db create error'
+    }else{
+      err = 'error! return null!'
+      console.log(err);
+    }
+    ctx.status = 200
+    ctx.body = {
+      code: 200,
+      msg: !err ? 'ok' : '',
+      data: {
+        res,
+        params
+      }
     }
   })
 }
