b7ee877447845e5a71a7d6ee474b7f00c911869c
LIYI <liyi@rixingyike.com>
2020-12-08 16:36:55 +0800

    2020-12-08:

--- 第6章/miniprogram/pages/my/index.js
+++ 第6章/miniprogram/pages/my/index.js
@@ -5,14 +5,60 @@ Page({
    * 页面的初始数据
    */
   data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    currentUserDocId:''
+  },
 
+  // 以下关于云登录
+
+  onGetUserInfo: function (e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function () {
+    // data:传递给云函数的参数，在云函数中可通过 event 参数获取
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('user openid: ', res.result.openid)
+        // 全局globalData已有
+        getApp().globalData.openid = res.result.openid
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
